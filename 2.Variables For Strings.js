194e61df90a093f61a98dcd2a8fdb16114e9fa36
Mohammad Talha <m.talhaleo2002@gmail.com>
2023-09-29 02:14:31 +0500

    Variables For Strings in Js

---
+++ 2.Variables For Strings.js
@@ -0,0 +1,27 @@
+// A variable is created when you write var (for variable) followed by the name that you choose to give it. It takes on a particular value when you assign the value to it. This is a JavaScript statement that creates the variable name and assigns the value "Mark" to it.
+
+
+var gender = "Male";
+console.log(gender);
+
+var city = "Melbourne";
+city = "London";
+console.log(city);
+
+var nationality;
+console.log(nationality);
+
+var weather;
+weather = "winter";
+console.log(weather);
+
+
+var floogie = "Mark";
+var toggle = "Mark";
+console.log(floogie);
+console.log(toggle);
+
+
+var time = "Three";
+var time = "five";
+console.log(time);
