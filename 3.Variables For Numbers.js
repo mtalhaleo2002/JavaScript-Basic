10581d8571d9c503f212c151fdfd6aa70f087b78
Mohammad Talha <m.talhaleo2002@gmail.com>
2023-09-29 02:32:01 +0500

    Variables For Numbers in Js

---
+++ 3.Variables For Numbers.js
@@ -0,0 +1,29 @@
+// A string isn't the only thing you can assign to a variable. You can also assign a number.
+
+var weight = 150;
+console.log(weight);
+
+x = weight + 25;
+console.log(x);
+
+var age = "30";
+console.log(age);
+
+var num = "35";
+newNum = num + 65;
+console.log(newNum);
+
+var quantity = 50;
+var dozen = 17;
+var wholesomme = quantity + dozen;
+console.log(wholesomme);
+
+
+var numeric = 67;
+numeric = numeric + 33;
+console.log(numeric);
+
+var value = "23";
+var newValue = value + 56;
+console.log(newValue);
+
