e1c48122c4e2dde74fda1393036aa14adddaa2c2
Mohammad Talha <m.talhaleo2002@gmail.com>
2023-09-29 17:12:05 +0500

    Concatenating Text Strings in Js

---
+++ 8.Conacatenating Text Strings.js
@@ -0,0 +1,16 @@
+
+var userName = "John Smith"
+console.log("Thanks, " + userName + "!");
+
+var message = "Thanks, ";
+var banger = "!";
+console.log(message + userName + banger);
+
+var message = "Thanks, ";
+var banger = "!";
+var userName = "Susan";
+var customMess = message + userName + banger;
+console.log(customMess);
+
+console.log("2" + "2");
+console.log("2 plus 2 equals " + 2 + 2);
