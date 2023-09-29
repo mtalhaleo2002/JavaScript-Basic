331f48b6717040701b4139f660977f758da4884a
Mohammad Talha <m.talhaleo2002@gmail.com>
2023-09-29 16:34:50 +0500

    Math Expressions: Unfamiliar Operators in Js

---
+++ 6.Math Expressions Unfamiliar Operators.js
@@ -0,0 +1,42 @@
+// There are several specialized math expressions you need to know. Here's the first one.
+
+var num = 5;
+console.log(num);
+
+num++;
+console.log(num);
+// This is a short way of writing...
+
+num = num + 1;
+console.log(num);
+// It increments the variable by 1
+
+// You decrement using minuses instead of pluses.
+num--;
+console.log(num);
+
+var digit = 1
+var newNum = digit++; //Post increment // 2 but prints 1 
+console.log(newNum);
+
+var newNumber = ++digit; //Pre Increment // prints 3
+console.log(newNumber);
+
+var constant = 2;
+console.log(constant)
+
+constant++;
+console.log(constant)
+
+++constant;
+console.log(constant)
+
+--constant;
+console.log(constant)
+
+constant--;
+console.log(constant)
+
+
+
+
