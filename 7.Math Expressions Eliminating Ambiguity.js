edd14885981502e04fde58f64d7bd8d710e268dc
Mohammad Talha <m.talhaleo2002@gmail.com>
2023-09-29 16:55:13 +0500

    Math Expressions: Eliminating Ambiguity in Js

---
+++ 7.Math Expressions Eliminating Ambiguity.js
@@ -0,0 +1,27 @@
+
+var count = 1 + 3 * 4;
+console.log(count);
+
+var count = 1 + (3 * 4);
+console.log(count);
+
+var count = (1 + 3) * 4;
+console.log(count);
+
+var resultOfComputation = (2 * 4) * 4 + 2;
+console.log(resultOfComputation);
+
+resultOfComputation = ((2 * 4) * 4) + 2;
+console.log(resultOfComputation);
+
+resultOfComputation = (2 * 4) * (4 + 2);
+console.log(resultOfComputation);
+
+
+
+
+
+
+
+
+
