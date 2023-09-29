17230f1d0e63cd0b02a53aee3c31aa8bb68bd20c
Mohammad Talha <m.talhaleo2002@gmail.com>
2023-09-29 14:47:52 +0500

    Variables Names Legal And Illegal in Js

---
+++ 4.Variable Names Legal And Illegal.js
@@ -0,0 +1,16 @@
+// A variable name can contain only letters, numbers, dollar signs, and underscores.
+// Though a variable name can't be any of JavaScript's keywords, it can contain keywords.
+// For example, userAlert and myVar are legal.
+// Capital letters are fine, but be careful. Variable names are case sensitive. A rose is not a
+// Rose. If you assign the string "Floribundas" to the variable rose, and then ask JavaScript for the value assigned to Rose, you'll come up empty.
+// I teach the camelCase naming convention. Why "camelCase"? Because there is a hump or two (or three) in the middle if the name is formed by more than one word.
+// A camelCase name begins in lower case. If there's more than one word in the name, each subsequent word gets an initial cap, creating a hump. 
+// If you form a variable name with only one  word, like response, there's no hump. It's a camel that's out of food. 
+// Please adopt the camelCase convention. It'll make your variable names more readable, and you'll be less likely to get variable names mixed up.
+
+
+// Examples:
+// userResponse
+// userResponseTime
+// userResponseTimeLimit
+// response
