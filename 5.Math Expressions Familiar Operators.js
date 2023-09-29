3c416782df4e8c074378b4707ffdd2d6f9ff8a09
Mohammad Talha <m.talhaleo2002@gmail.com>
2023-09-29 15:46:50 +0500

    Math Expressions: Familiar Operators in Js

---
+++ 5.Math Expressions Familiar Operators.js
@@ -0,0 +1,32 @@
+
+var num = 4;
+console.log(num);
+
+var digit = 2 + 2;
+console.log(digit);
+
+var sum = 24 - 65;
+console.log(sum);
+
+var quantity = 3 * 56;
+console.log(quantity);
+
+var number = 10;
+var newNum = number + 56;
+console.log(newNum);
+
+var first = 98;
+var second = 102;
+var final = first + second;
+console.log(final);
+
+var leftOver = 10 % 3;
+console.log(leftOver);
+
+// % is the modulus operator. It doesn't give you the result of dividing one number by another.
+// It gives you the remainder when the division is executed.
+// If one number divides evenly into another, the modulus operation returns 0.
+// In the following statement, 0 is assigned to the variable.
+
+var count = 9 % 3;
+console.log(count);
