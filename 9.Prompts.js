0733989e065fd058e6fd8fc5022a556196a7d80a
Mohammad Talha <m.talhaleo2002@gmail.com>
2023-09-30 07:38:11 +0500

    Prompts in Js

---
+++ 9.Prompts.js
@@ -0,0 +1,9 @@
+// A prompt box asks the user for some information and provides a response field for her answer.
+
+var spec = prompt("Your species?", "human");
+
+console.log(spec); //It runs when you attch html file .
+
+// Prompt code is like alert code, with two differences. In a prompt, you need a way to capture the user's response. That means you need to start by declaring a variable, followed by an equal sign.
+// In a prompt, you can specify a second string. This is the default response that appears in the field when the prompt displays. If the user leaves the default response as-is and just clicks OK, the default response is assigned to the variable. It's up to you whether you include a default response
+
